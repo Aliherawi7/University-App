c7614e0ba2fe04fc1826142ea825fe767ea85424
ali herawi <aliherawi7@gmail.com>
2023-05-26 22:12:13 +0430

    three layer created for student entity

--- backend/student-service/src/main/java/com/mycompany/models/Student.java
+++ backend/student-service/src/main/java/com/mycompany/models/Student.java
@@ -1,4 +1,36 @@
 package com.mycompany.models;
 
+import com.mycompany.constants.Gender;
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@AllArgsConstructor()
+@Getter
+@Setter
+@Data
+@Builder
 public class Student {
+    @Id
+    @SequenceGenerator(name = "student-sequence", initialValue = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "student-sequence")
+    private Long id;
+    private String name;
+    private String lastName;
+    private LocalDate dob;
+    private String nationality;
+    private String phoneNumber;
+    private Gender gender;
+    private LocalDate joinedDate;
+    private String nationalId;
+    private String imageUrl;
+    private String location;
+    private Long fieldOfStudyId;
+    private Long departmentId;
+    private String email;
+    private String password;
+
 }
+
