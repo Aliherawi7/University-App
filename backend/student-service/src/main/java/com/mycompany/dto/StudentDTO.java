56e6a7b564f479e852544039a609b363d40ccb9c
ali herawi <aliherawi7@gmail.com>
2023-05-26 22:14:26 +0430

    this dto created for sending the student inforamtion to the client side

--- backend/student-service/src/main/java/com/mycompany/dto/StudentDTO.java
+++ backend/student-service/src/main/java/com/mycompany/dto/StudentDTO.java
@@ -1,4 +1,29 @@
 package com.mycompany.dto;
 
+import com.mycompany.constants.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@Data
+@Builder
+@NoArgsConstructor
 public class StudentDTO {
+    private String name;
+    private String lastName;
+    private String email;
+    private String imageUrl;
+    private LocalDate dob;
+    private String nationality;
+    private String phoneNumber;
+    private Gender gender;
+    private LocalDate joinedDate;
+    private String nationalId;
+    private String location;
+    private String fieldOfStudy;
+    private String departmentName;
 }
