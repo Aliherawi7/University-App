2a43007c9e5b61ae04f9ab3d8375f05beb05e0ce
ali herawi <aliherawi7@gmail.com>
2023-05-26 22:11:33 +0430

    gender implemented

--- backend/student-service/src/main/java/com/mycompany/constants/Gender.java
+++ backend/student-service/src/main/java/com/mycompany/constants/Gender.java
@@ -1,4 +1,17 @@
 package com.mycompany.constants;
 
 public enum Gender {
+    MALE("MALE"),
+    FEMALE("FEMALE"),
+    OTHER("OTHER");
+
+    final String value;
+
+    Gender(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
