a066fd0a5ee6630614fc274aab5106d91ef7d27d
ali herawi <aliherawi7@gmail.com>
2023-05-28 11:50:33 +0430

    enums added for Degree and Gender values

--- backend/teacher-service/src/main/java/com/mycompany/constants/Degree.java
+++ backend/teacher-service/src/main/java/com/mycompany/constants/Degree.java
@@ -1,2 +1,17 @@
-package com.mycompany.constants;public enum Degree {
+package com.mycompany.constants;
+
+public enum Degree {
+    AS("Associate's Degree"),
+    BS("Bachelor's Degree"),
+    MA("Mater's Degree"),
+    PhD("Doctorate Degree");
+
+    private final String value;
+
+    Degree(String value) {
+        this.value = value;
+    }
+    public String getValue() {
+        return value;
+    }
 }
--- backend/teacher-service/src/main/java/com/mycompany/constants/Gender.java
+++ backend/teacher-service/src/main/java/com/mycompany/constants/Gender.java
@@ -1,2 +1,15 @@
-package com.mycompany.constants;public enum Gender {
+package com.mycompany.constants;
+
+public enum Gender {
+    MALE("MALE"),
+    FEMALE("FEMALE"),
+    OTHER("OTHER");
+   private final String value;
+
+    Gender(String value) {
+        this.value = value;
+    }
+    public String getValue() {
+        return value;
+    }
 }
